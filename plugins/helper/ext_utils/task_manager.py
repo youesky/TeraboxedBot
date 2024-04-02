27e075b06e878d0da822901f860d110a0579e7e1
youesky <fousiyatc@gmail.com>
2024-04-02 23:27:33 +0530

    update task_manager.py

--- plugins/helper/ext_utils/task_manager.py
+++ plugins/helper/ext_utils/task_manager.py
@@ -5,7 +5,7 @@ from plugins.helper.mirror_utils.download_utils.direct_link_generator import ter
 
 
 async def format_message(link_data):
-    download_link, title, total_size = await terabox(link_data)
+    download_link, title, total_size = terabox(link_data)
     file_name = f"<a href={link_data}>{title}</a>"
     file_size = get_readable_file_size(total_size)
     return f"┎ <b>Title</b>: {file_name}\n┠ <b>Size</b>: <code>{file_size}</code>\n┖ <b>Link</b>: <a href={download_link}>Link</a>"
