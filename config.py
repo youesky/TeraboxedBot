5668ab8e19de4a7f95ccc2891fd708834a62dcfe
youesky <fousiyatc@gmail.com>
2024-04-02 05:33:07 +0530

    update config.py

--- config.py
+++ config.py
@@ -5,5 +5,5 @@ API_HASH = getenv("API_HASH", "02ce7c339f7776844ff4ab03da338ccd")
 BOT_TOKEN = getenv("BOT_TOKEN", "7164272870:AAHbHvoWGurO1IS1L34Nv-tM8nurgNuwcsA")
 OWNER_ID = int(getenv("OWNER_ID", "1034599258")) 
 
-LOG_CHANNEL = getenv("LOG_CHANNEL", "")
+LOG_CHANNEL = getenv("LOG_CHANNEL", "1034599258")
 LOG_MSG = "{} Iꜱ Rᴇsᴛᴀʀᴛᴇᴅ....✨\n\n🗓️ Dᴀᴛᴇ : {}\n⏰ Tɪᴍᴇ : {}\n\n🖥️ Rᴇᴘᴏ: {}\n🉐 Vᴇʀsɪᴏɴ: {}\n🧾 Lɪᴄᴇɴꜱᴇ: {}\n©️ Cᴏᴩʏʀɪɢʜᴛ: {}"
