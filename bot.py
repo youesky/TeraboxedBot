f228b01a43b09a291413537c2970c99e00e93896
youesky <fousiyatc@gmail.com>
2024-04-02 18:13:12 +0530

    update bot.py

--- bot.py
+++ bot.py
@@ -40,13 +40,13 @@ class Bot(Client):
         self.name = me.first_name
         self.mention = me.mention
         self.username = me.username
-        self.admins = ADMINS
+        self.admins = ADMINS[0]
         self.uptime = time.time()
         curr = datetime.datetime.now(pytz.timezone("Asia/Kolkata"))
         date = curr.strftime('%d %B, %Y')
         tame = curr.strftime('%I:%M:%S %p')
         logger.info(LOG_MSG.format(me.first_name, date, tame, __repo__, __version__, __license__, __copyright__))
-        try: await self.send_message(ADMINS, LOG_MSG.format(me.first_name, date, tame, __repo__, __version__, __license__, __copyright__), disable_web_page_preview=True)   
+        try: await self.send_message(ADMINS[0], LOG_MSG.format(me.first_name, date, tame, __repo__, __version__, __license__, __copyright__), disable_web_page_preview=True)   
         except Exception as e: logger.warning(f"Bot Isn't Able To Send Message To ADMINS \n{e}")
         if WEBHOOK is True:
             app = web.AppRunner(await web_server())
