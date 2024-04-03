94d1d48c9bfeb1b09fd36da0b9e320ebbff6e807
youesky <fousiyatc@gmail.com>
2024-04-03 07:06:32 +0530

    update utils.py

--- utils.py
+++ utils.py
@@ -1,8 +1,8 @@
 class temp(object):
     U_NAME = None
     B_NAME = None
-    
-__repo__ = "https://github.com/youesky/TeraboxedBot"
+ 
+__repo__ = "<a href='https://github.com/youesky/TeraboxedBot'>TeraboxedBot</a>"
 __version__ = "TeraboxedBot ᴠ1.1.0"
 __license__ = "GNU GENERAL PUBLIC LICENSE V2"
 __copyright__ = "Copyright (C) 2024-present @ebiza - <a href='https://github.com/youesky'>youesky</a>"
